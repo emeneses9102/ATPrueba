9774c14a9c9280e9986ca6385ce9fcc458679c8e
emenesesc13 <edgarmc9102@gmail.com>
2022-07-23 01:18:26 -0500

    primer commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+require_once 'controller/plantilla.controller.php';
+
+//require_once 'modelo/alumnos.modelo.php';
+
+$plantilla = new ControllerPlantilla();
+$plantilla->ctrPlantilla();
+//cambios 22
---
+++ views/plantilla.php
@@ -0,0 +1,35 @@
+<?php 
+//inicio la session    
+//session_start();
+/*session_start([
+	'cookie_lifetime' => 864000,
+]);
+*/
+//session_set_cookie_params(60*60*24);
+//Validamos si el usuario ha iniciado sesión
+if(isset($_SESSION['iniciarSesion']) && $_SESSION['iniciarSesion'] == "ok")
+	{
+		require_once 'views/modulos/header.php';
+		echo '<main class="app-content">';
+		//-- Contenido --
+			if(isset($_GET['ruta'])){
+				
+				if($_GET['ruta'] == "inicio" 
+					){
+					include_once "views/modulos/".$_GET['ruta'].".php";
+				}
+				else{
+					include_once "views/modulos/404.php";
+				}
+			}
+			else{
+				include_once "views/modulos/inicio.php";
+			}
+
+		echo '</main>';
+		require_once 'views/modulos/footer.php';
+	}
+	//Si no ha iniciado es redirigido al login
+else{
+	require_once "views/modulos/login.php";
+}
